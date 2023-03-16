bd8bd2fcbaa5c2798484eec2076191bbd753c2cd
Jules <110929073+juse122@users.noreply.github.com>
2023-03-16 15:37:09 +0100

    Add persistent dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,33 @@
+let buttonState = "";
 const darkModeButton = document.querySelector("#footer-darkmode-switch");
-darkModeButton.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+const metaTag = document.querySelector("meta[name=theme-color]");
 
-    const metaTag = document.querySelector("meta[name=theme-color]");
-    const currentColor = metaTag.getAttribute("content");
+const getMode = () => {
+    buttonState = localStorage.getItem("buttonState");
 
-    if (currentColor === "#afc6ff") {
+    if (buttonState === null) {
+        localStorage.setItem("buttonState", "lightMode");
+    } else if (buttonState === "darkMode") {
+        document.body.classList.add("dark-mode");
         metaTag.setAttribute("content", "#003264");
-    } else {
+        darkModeButton.checked = true;
+    };
+};
+
+getMode();
+
+const changeMode = () => {
+    if (!darkModeButton.checked) {
+        document.body.classList.remove("dark-mode");
+        localStorage.setItem("buttonState", "lightMode");
         metaTag.setAttribute("content", "#afc6ff");
+    } else {
+        document.body.classList.add("dark-mode");
+        localStorage.setItem("buttonState", "darkMode");
+        metaTag.setAttribute("content", "#003264");
     }
+};
+
+darkModeButton.addEventListener("click", () => {
+    changeMode();
 });
