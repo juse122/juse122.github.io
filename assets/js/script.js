9bfa8b2a8aa79919866b047e04d6dc7233750e50
Jules <110929073+juse122@users.noreply.github.com>
2023-04-18 14:10:47 +0200

    Update subpages with new design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Dark Mode
 let buttonState = "";
 const darkModeButton = document.querySelector("#footer-darkmode-switch");
 const metaTag = document.querySelector("meta[name=theme-color]");
@@ -31,3 +32,16 @@ const changeMode = () => {
 darkModeButton.addEventListener("click", () => {
     changeMode();
 });
+
+
+
+// DOB calculator + footer year setter
+const initialDate = new Date("07/18/1990");
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const timeDifference = currentDate.getTime() - initialDate.getTime();
+const dayDifference = (timeDifference + 100000000) / (1000 * 3600 * 24);
+const currentAge = Math.floor(dayDifference / 365.25);
+
+document.querySelector("#current-year").textContent = currentYear;
+document.querySelector("#current-age").textContent = currentAge;
