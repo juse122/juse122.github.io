5d64a86ae534ad71cdf2a6c4c4034b75c9f8ee7a
Jules <110929073+juse122@users.noreply.github.com>
2023-11-27 09:46:51 +0100

    Add new project and format code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,36 +4,34 @@ const darkModeButton = document.querySelector("#footer-darkmode-switch");
 const metaTag = document.querySelector("meta[name=theme-color]");
 
 const getMode = () => {
-    buttonState = localStorage.getItem("buttonState");
+	buttonState = localStorage.getItem("buttonState");
 
-    if (buttonState === null) {
-        localStorage.setItem("buttonState", "lightMode");
-    } else if (buttonState === "darkMode") {
-        document.body.classList.add("dark-mode");
-        metaTag.setAttribute("content", "#003264");
-        darkModeButton.checked = true;
-    };
+	if (buttonState === null) {
+		localStorage.setItem("buttonState", "lightMode");
+	} else if (buttonState === "darkMode") {
+		document.body.classList.add("dark-mode");
+		metaTag.setAttribute("content", "#003264");
+		darkModeButton.checked = true;
+	}
 };
 
 getMode();
 
 const changeMode = () => {
-    if (!darkModeButton.checked) {
-        document.body.classList.remove("dark-mode");
-        localStorage.setItem("buttonState", "lightMode");
-        metaTag.setAttribute("content", "#afc6ff");
-    } else {
-        document.body.classList.add("dark-mode");
-        localStorage.setItem("buttonState", "darkMode");
-        metaTag.setAttribute("content", "#003264");
-    }
+	if (!darkModeButton.checked) {
+		document.body.classList.remove("dark-mode");
+		localStorage.setItem("buttonState", "lightMode");
+		metaTag.setAttribute("content", "#afc6ff");
+	} else {
+		document.body.classList.add("dark-mode");
+		localStorage.setItem("buttonState", "darkMode");
+		metaTag.setAttribute("content", "#003264");
+	}
 };
 
 darkModeButton.addEventListener("click", () => {
-    changeMode();
+	changeMode();
 });
-
-
 
 // DOB calculator + footer year setter
 const initialDate = new Date("07/18/1990");
